c9d972991522283410c26bccdd5874e344d832da
Holivane <holivane@gmail.com>
2021-12-04 12:14:39 -0300

    Criação da primeira requisição

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const config = require('config')
 
 app.use(bodyParser.json())
 
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador)
+
 app.listen(config.get('api.porta'), () => {
     console.log('A API está funcionando')
 })

d6ef7c07e9ccc66a4fd483a8e3213d15fab222ff
Holivane <holivane@gmail.com>
2021-12-13 16:47:19 -0300

    Criação de rotas para post e get por id

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,11 +1,38 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar()
     res.send(
         JSON.stringify(resultados)
     )
+})
+
+roteador.post('/', async (req, res) => {
+    const dadosrecebidos = req.body
+    const fornecedor = new Fornecedor(dadosrecebidos)
+    await fornecedor.criar()
+    res.send(
+        JSON.stringify(fornecedor)
+    )
+})
+
+roteador.get('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        res.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch (erro) {
+        res.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        )
+    }
 })
 
 module.exports = roteador
